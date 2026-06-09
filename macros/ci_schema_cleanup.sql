84b623b4e4de9d1fa0f8da4a8d115501c43de7a1
shola007 <oipoade@yahoo.com>
2026-06-08 23:42:09 -0400

    Adding a workflow for cleaning up schemas in CI database

---
+++ macros/ci_schema_cleanup.sql
@@ -0,0 +1,32 @@
+{# 
+    This macro finds PR schemas older than a set date and drops them 
+    The macro defaults to 10 days old, but can be configured with the input argument age_in_days
+    Sample usage with different date:
+        dbt run-operation pr_schema_cleanup --args "{'database_to_clean': 'analytics','age_in_days':'15'}"
+#}
+{% macro pr_schema_cleanup(database_to_clean, age_in_days=-1) %}
+
+    {% set find_old_schemas %}
+        select 
+            'drop schema {{ database_to_clean }}.'||schema_name||';'
+        from {{ database_to_clean }}.information_schema.schemata
+        where
+            catalog_name = '{{ database_to_clean | upper }}'
+            and schema_name ilike 'PR%'
+            and last_altered <= (current_date() - interval '{{ age_in_days }} days')
+    {% endset %}
+
+    {% if execute %}
+
+        {{ log('Schema drop statements:' ,True) }}
+
+        {% set schema_drop_list = run_query(find_old_schemas).columns[0].values() %}
+
+        {% for schema_to_drop in schema_drop_list %}
+            {% do run_query(schema_to_drop) %}
+            {{ log(schema_to_drop ,True) }}
+        {% endfor %}
+
+    {% endif %}
+
+{% endmacro %}
