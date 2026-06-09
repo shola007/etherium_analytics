da1db25094699ebbbaa508034312a596e51ef3ce
shola007 <oipoade@yahoo.com>
2026-06-08 22:00:48 -0400

    corrected the sql error in int transaction enriched

--- models/intermediate/analytics/int_transactions_enriched.sql
+++ models/intermediate/analytics/int_transactions_enriched.sql
@@ -30,7 +30,7 @@ select
     t.receipt_contract_address,
     t.input,
     tt.token_transfers_count,
-    1 as new_field
+    1 as new_field,
 case
     when t.receipt_contract_address != '' then 'contract_creation'
     when tt.transaction_hash is not null then 'token_transfer'
